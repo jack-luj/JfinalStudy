ee244a1a52ccf8d9dc48a840e7ac5c7f17e472df
jack-luj <wjsc@qq.com>
2016-05-11 11:11:48 +0800

    init

---
+++ JfinalJetty/src/demo/DemoConfig.java
@@ -0,0 +1,27 @@
+package demo;
+
+import com.jfinal.config.*;
+
+/**
+ * Created by jackl on 2016/5/11.
+ */
+public class DemoConfig extends JFinalConfig{
+
+    public void configConstant(Constants me) {
+    me.setDevMode(true);
+    }
+
+    public void configRoute(Routes me) {
+        me.add("/hello",HelloController.class);
+        me.add("/",Indexcontroller.class);
+    }
+     public void configPlugin(Plugins me) {
+
+    }
+     public void configInterceptor(Interceptors me) {
+
+    }
+     public void configHandler(Handlers me) {
+
+    }
+}
---
+++ JfinalTomcat/src/com/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+import com.jfinal.core.Controller;
+
+/**
+ * Created by jackl on 2016/5/11.
+ */
+public class HelloController extends Controller{
+public void index(){
+    renderText("欢迎来到Jfinal");
+}
+}
